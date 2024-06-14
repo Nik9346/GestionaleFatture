3fabda05a47186e4a783c6f14423ac2deb09a14c
Nicola Pasquarelli <nicola.pasquarelli46@gmail.com>
2024-06-14 11:20:08 +0200

    Modified Env var db and password

--- src/it/stopAndCode/repository/Repository.java
+++ src/it/stopAndCode/repository/Repository.java
@@ -5,16 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public interface Repository {
-	String URL = "jdbc:mysql://localhost:3310/dbgestionefatture"; // jdbc:mysql driver per il database di mysql --
+	String MAMPPORT = "8889";
+	String XAAMPPORTS = "3310";
+	String URL = "jdbc:mysql://localhost:"+MAMPPORT+"/dbgestionefatture"; // jdbc:mysql driver per il database di mysql --
 	// indirizzo del server / nome del database con
 	// il quale si vuole interagire "sql_01"
 	String USERNAME = "root";
 	String PASSWORD = null;
+	String MAMPPASSWORD = "root";
 
 // metodo ausiliario che ritorna la connessione con il database
 	default Connection getConnection() { // lo facciamo diventare un metodo defaul
 		try {
-			Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			Connection connection = DriverManager.getConnection(URL, USERNAME, MAMPPASSWORD);
 			return connection;
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
