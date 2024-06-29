6d223030122f1f95a2cdd953b087c8e4bd62a5fa
Nicola Pasquarelli <nicola.pasquarelli46@gmail.com>
2024-06-29 17:35:21 +0200

    Tolto import java.sql.connection in main

--- src/it/stopAndCode/eseguibile/Main.java
+++ src/it/stopAndCode/eseguibile/Main.java
@@ -3,7 +3,7 @@ package it.stopAndCode.eseguibile;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
